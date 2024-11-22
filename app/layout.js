16406ac87e2166a8baa2b98a083663bd2bb4dcdb
julietoge <julietogechi27@gmail.com>
2024-11-22 17:23:55 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "../styles/globals.css"
 import NavBar from "@/components/navBar";
 import Footer from "@/components/footer";
 
--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import styles from "./page.module.css";
+import styles from "../styles/page.module.css";
 import Link from "next/link";
 
 export default function Home() {
